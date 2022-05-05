0fe317c8372118dd35560e81b6da8ff3beb2a4a4
Thaikkoottathil Dhanasyam <92707155+Dhanasyam@users.noreply.github.com>
2022-05-05 22:28:14 +0800

    COMPLETED practical 2

--- MADPracc2/app/src/main/java/sg/edu/np/mad/madpracc2/MainActivity.java
+++ MADPracc2/app/src/main/java/sg/edu/np/mad/madpracc2/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.np.mad.madpracc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button follow = findViewById(R.id.FollowBtn);
+
+        follow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                follow.setText("unfollow");
+            }
+        });
     }
 }
